3e238b093f0b04fe56d97d157f4a3e9e94cf401a
Alana Li <ruiruialana@gmail.com>
2023-10-10 13:35:36 -0400

    Added zero reward consequence

--- q-learning.py
+++ q-learning.py
@@ -1,45 +1,41 @@
 import numpy as np
 import random
 
-gamma = 0.7 # Discount factor 
-alpha = 0.1 # Learning rate
-
-# Example rewards table
+"""
+Example matrix representation
 r = np.array([[0,1,1,1,0,0,0,0,0,0],
-					[0,0,0,0,1,0,0,0,0,0],
-					[0,1,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,1,1,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,1,1,0],
-					[0,0,0,0,0,0,0,0,0,1],
-					[0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,1,0,0],
-					[0,0,0,0,0,0,0,0,0,0]])
-
-# Example graph dictionary
-g =  {1:[1, 2, 10, 11], 2: [2, 3, 4, 5, 6, 7], 3: [3, 8, 9], 4: [4, 8, 9], 5: [5, 8, 9], 6: [6, 8, 9], 7: [7, 8, 9], 8: [8], 9: [9, 13], 10: [10],
-            11: [11, 12, 13], 12: [12], 13: [13, 14], 14: [14, 15, 16, 17, 18, 19], 15: [15, 20], 16: [16, 20], 17: [17, 21], 18: [18, 21], 19: [19, 21], 
-            20: [20, 22], 21: [21, 22], 22: [22, 23, 24, 25], 23: [23], 24: [24, 27], 25: [25, 26, 27], 26: [26], 27: [27, 28], 28: [27, 28, 29], 
-            29: [28, 29, 30, 32], 30: [29, 30, 31], 31: [30, 31], 32: [32]}
+				[0,0,0,0,1,0,0,0,0,0],
+				[0,1,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,1,1,0,0,0],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,1,1,0],
+				[0,0,0,0,0,0,0,0,0,1],
+				[0,0,0,0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0,1,0,0],
+				[0,0,0,0,0,0,0,0,0,0]])
+Example dictionary representation
+g = {1:[2, 3, 4], 2:[5], 3:[2], 4:[6, 7], 5:[], 6:[8, 9], 7:[10], 8:[], 9:[8], 10:[]}
+"""
 
 class QAgent():
     # Initialize alpha, gamma, rewards, and Q-values
-	def __init__(self, alpha, gamma, rewards, random=False, Q=[], risks=[]):
+	def __init__(self, alpha, gamma, graph, consequence=None, random=False, risks=None):
 		self.gamma = gamma  
 		self.alpha = alpha
-		self.rewards = self.create_rewards_matrix(rewards)
+		self.rewards = self.create_rewards_matrix(graph)
 		self.size = len(self.rewards[0])
+		self.consequence = consequence
 		if random:
 			self.generate_random_rewards(self.rewards)
-		if risks == []:
-			self.risks = self.generate_risks(self.rewards)
+		if consequence != None:
+			if risks != None:
+				self.risks = risks
+			else:
+				self.risks = self.generate_risks(self.rewards)
 		else:
 			self.risks = risks
 		self.actions = list(range(self.size))
-		if Q == []:
-			self.Q = np.random.rand(self.size, self.size) * 0
-		else:
-			self.Q = Q
+		self.Q = np.random.rand(self.size, self.size) * 0
 
 	def create_rewards_matrix(self, graph):
 		matrix = [[0 for _ in range(len(graph))] for _ in range(len(graph))]
@@ -76,7 +72,12 @@ class QAgent():
 					playable_actions.append(j)
 			if playable_actions:
 				next_state = np.random.choice(playable_actions)
-				TD = rewards_new[current_state][next_state] + self.gamma * np.max(self.Q[next_state, :]) - self.Q[current_state][next_state]
+				if self.consequence == "neg":
+					risk_reward = (1 - self.risks[current_state][next_state]) * rewards_new[current_state][next_state]
+				elif self.consequence == None:
+					risk_reward = rewards_new[current_state][next_state]
+
+				TD = risk_reward + self.gamma * np.max(self.Q[next_state, :]) - self.Q[current_state][next_state]
 				
 				self.Q[current_state][next_state] += self.alpha * TD
 		# Get the route 
@@ -96,8 +97,9 @@ class QAgent():
 	def print_helper(self, start, end, r):
 		print("Rewards Table:")
 		self.print_matrix(self.rewards)
-		print("Risk Table:")
-		self.print_matrix(self.risks)
+		if self.consequence != None:
+			print("Risk Table:")
+			self.print_matrix(self.risks)
 		print("Q-Table:")
 		self.print_matrix(self.Q)
 		print("Optimal path from", start, "to", end, ":")
@@ -115,5 +117,23 @@ class QAgent():
 		print()
 
 
-qagent = QAgent(alpha, gamma, g, random = False)
-qagent.training(1, 31, 100000)
+"""
+Possible Consequences:
+"neg": no reward
+None (default): No consequence
+"""
+alpha = 0.1 # Learning rate
+gamma = 0.7 # Discount factor
+
+g = {1:[1, 2, 10, 11], 2: [2, 3, 4, 5, 6, 7], 3: [3, 8, 9], 4: [4, 8, 9], 5: [5, 8, 9], 6: [6, 8, 9], 7: [7, 8, 9], 8: [8], 
+	9: [9, 13], 10: [10], 11: [11, 12, 13], 12: [12], 13: [13, 14], 14: [14, 15, 16, 17, 18, 19], 15: [15, 20], 16: [16, 20], 17: [17, 21], 18: [18, 21], 19: [19, 21], 20: [20, 22], 21: [21, 22], 22: [22, 23, 24, 25], 23: [23], 24: [24, 27], 25: [25, 26, 27], 26: [26], 27: [27, 28], 28: [27, 28, 29], 29: [28, 29, 30, 32], 30: [29, 30, 31], 31: [30, 31], 32: [32]}
+
+print("NO CONSEQUENCE")
+# 1 -> 11 -> 13 -> 14 -> 17 -> 21 -> 22
+qagent = QAgent(alpha, gamma, g)
+qagent.training(1, 22, 100000)
+
+print("\n\nNEG CONSEQUENCE")
+# 1 -> 11 -> 13 -> 14 -> 16 -> 20 -> 22
+qagent = QAgent(alpha, gamma, g, consequence="neg")
+qagent.training(1, 22, 100000)
