20355f23d1ea4aebf14c9df79e44497173df8983
Alana Li <ruiruialana@gmail.com>
2023-10-11 10:51:27 -0400

    Returns when path cannot be found
    Catches failed paths instead of looping infinitely

--- q-learning.py
+++ q-learning.py
@@ -17,6 +17,8 @@ Example dictionary representation
 g = {1:[2, 3, 4], 2:[5], 3:[2], 4:[6, 7], 5:[], 6:[8, 9], 7:[10], 8:[], 9:[8], 10:[]}
 """
 
+MAX_RUNS = 10
+
 class QAgent():
     # Initialize alpha, gamma, rewards, and Q-values
 	def __init__(self, alpha, gamma, graph, consequence=None, random=False, risks=None):
@@ -78,32 +80,38 @@ class QAgent():
 					risk_reward = rewards_new[current_state][next_state]
 
 				TD = risk_reward + self.gamma * np.max(self.Q[next_state, :]) - self.Q[current_state][next_state]
-				
 				self.Q[current_state][next_state] += self.alpha * TD
-		# Get the route 
 		self.get_optimal_route(start_location, end_location, self.Q)
 		
-	# Get the optimal route
 	def get_optimal_route(self, start_location, end_location, Q):
 		current_state = start_location
 		route = [str(current_state + 1)]
 		next_state = 0
-		while(next_state != end_location):
+		count = 0
+		while(next_state != end_location) and count < MAX_RUNS:
 			next_state = np.argmax(Q[current_state, ])
 			route.append(str(next_state + 1))
 			current_state = next_state
-		self.print_helper(start_location + 1, end_location + 1, route)
+			count += 1
+		success = True if (end_location + 1) == int(route[-1]) else False
+		self.print_helper(start_location + 1, end_location + 1, route, success)
 	
-	def print_helper(self, start, end, r):
-		print("Rewards Table:")
-		self.print_matrix(self.rewards)
-		if self.consequence != None:
-			print("Risk Table:")
-			self.print_matrix(self.risks)
-		print("Q-Table:")
-		self.print_matrix(self.Q)
-		print("Optimal path from", start, "to", end, ":")
-		print(" -> ".join(r))
+	def print_helper(self, start, end, r, success):
+		if success:
+			print("Rewards Table:")
+			self.print_matrix(self.rewards)
+			# Risk table only applicable when there are consequences
+			if self.consequence != None:
+				print("Risk Table:")
+				self.print_matrix(self.risks)
+			print("Q-Table:")
+			self.print_matrix(self.Q)
+			print("Optimal path from", start, "to", str(end) + ":")
+			print(" -> ".join(r))
+		else:
+			print("FAILED")
+			print("Could not find path from", start, "to", end)
+			print(" -> ".join(r))
 
 	# Helper function for displaying matrix
 	def print_matrix(self, matrix):
@@ -129,11 +137,9 @@ g = {1:[1, 2, 10, 11], 2: [2, 3, 4, 5, 6, 7], 3: [3, 8, 9], 4: [4, 8, 9], 5: [5,
 	9: [9, 13], 10: [10], 11: [11, 12, 13], 12: [12], 13: [13, 14], 14: [14, 15, 16, 17, 18, 19], 15: [15, 20], 16: [16, 20], 17: [17, 21], 18: [18, 21], 19: [19, 21], 20: [20, 22], 21: [21, 22], 22: [22, 23, 24, 25], 23: [23], 24: [24, 27], 25: [25, 26, 27], 26: [26], 27: [27, 28], 28: [27, 28, 29], 29: [28, 29, 30, 32], 30: [29, 30, 31], 31: [30, 31], 32: [32]}
 
 print("NO CONSEQUENCE")
-# 1 -> 11 -> 13 -> 14 -> 17 -> 21 -> 22
 qagent = QAgent(alpha, gamma, g)
 qagent.training(1, 22, 100000)
 
 print("\n\nNEG CONSEQUENCE")
-# 1 -> 11 -> 13 -> 14 -> 16 -> 20 -> 22
 qagent = QAgent(alpha, gamma, g, consequence="neg")
 qagent.training(1, 22, 100000)
